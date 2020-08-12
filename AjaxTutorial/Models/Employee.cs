0914565646ab7c315f7204dc53e03a39bba019dc
Ashton Wray <aricks1986@gmail.com>
2020-08-12 03:34:20 -0400

    github desktop setup
    getting github desktop setup working

--- AjaxTutorial/Models/Employee.cs
+++ AjaxTutorial/Models/Employee.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Web;
 
@@ -10,10 +12,14 @@ namespace AjaxTutorial.Models
     public partial class Employee
     {
         public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string Name { get; set; }
         public string Position { get; set; }
         public string Office { get; set; }
         public Nullable<int> Salary { get; set; }
+
+        [DisplayName("Image")]
         public string ImagePath { get; set; }
 
         [NotMapped]
@@ -21,7 +27,7 @@ namespace AjaxTutorial.Models
 
         public Employee()
         {
-            ImagePath = "~/AppFiles/Images/attackonkitty.jpg";
+            ImagePath = "/AppFiles/Images/attackonkitty.jpg";
          
         }
     }
---
+++ AjaxTutorial/Scripts/Scripts.js
@@ -0,0 +1,24 @@
+﻿function ShowImagePreview(imageUploader, previewImage) {
+    if (imageUploader.files && imageUploader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(imageUploader.files[0]);
+    }
+}
+
+function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+        $.ajax({
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                $("#firstTab").html(response);
+            }
+        });
+    }
+    return false;
+}
